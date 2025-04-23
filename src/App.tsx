7c8082d5b144ef15eba18d474e49b914c48df396
Rakesh Das <rakesh@ovntech.com>
2025-04-23 15:48:18 +0530

    clean up

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 // src/App.js
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import HomePage from "./page/HomePage";
 import PortfolioPage from "./page/PortfolioPage";
 
 const App = () => {
