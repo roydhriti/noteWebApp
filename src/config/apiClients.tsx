eb50a827ed59a424e05699fb0f428ec9d29ec89d
Rakesh Das <rakesh@ovntech.com>
2025-04-23 23:36:07 +0530

    adding live url

--- src/config/apiClients.tsx
+++ src/config/apiClients.tsx
@@ -1,6 +1,7 @@
 // const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 // const API_BASE_URL = "http://209.105.242.30:7777";
-const API_BASE_URL = "http://127.0.0.1:8000";
+// const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://dynamic-porfolioapi-2.onrender.com";
 
 // this is using for json formate
 
