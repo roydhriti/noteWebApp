46e9ba6d1d4c986377ec02952ac33c7852fc5cd6
Rakesh Das <rakesh@ovntech.com>
2025-04-24 23:43:56 +0530

    fix

--- src/config/apiClient.tsx
+++ src/config/apiClient.tsx
@@ -1,7 +1,7 @@
 //  const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
 // const API_BASE_URL = "http://209.105.242.30:7777";
-const API_BASE_URL = "http://127.0.0.1:8000";
-// const API_BASE_URL = "https://dynamic-porfolioapi-2.onrender.com";
+// const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "https://dynamic-porfolioapi-2.onrender.com";
 
 if (!API_BASE_URL) {
   throw new Error(
